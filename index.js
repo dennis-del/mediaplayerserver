8859846ba226ad3bf452381ff81ba861f2e4002e
dennis-del <dennisjames991@gmail.com>
2024-06-26 09:36:48 +0530

    final version of server

---
+++ index.js
@@ -0,0 +1,24 @@
+//1.import json server
+
+const jsonServer = require('json-server')
+
+// 2.create path for db.json file using router function - for storing data
+const router = jsonServer.router("db.json")
+
+//3.create middleware to convert json to js - defaults()
+const middleware = jsonServer.defaults()
+
+//4.create json server
+const mediaPlayerServer = jsonServer.create()
+
+//5.server should use middleware and router
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+//6.set port for server
+const PORT = 9700 || process.env.PORT
+
+//7. run server
+mediaPlayerServer.listen(PORT,()=>{
+    console.log(`server running successfully at port number ${PORT}`);
+})
